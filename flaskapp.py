5b3e4ce99263c860ed6b4ad20c8fb0d2580aeae6
Jennifer Deng <62663431+jennifergithub@users.noreply.github.com>
2020-07-23 12:28:10 -0400

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask import request
 from nltk.corpus import wordnet
 from nltk.corpus import stopwords
@@ -6,10 +6,10 @@ from nltk.tokenize import RegexpTokenizer
 import random
 
 app = Flask(__name__)
+tokenizer = RegexpTokenizer(r'\w+')
 
 @app.route("/word", methods=["GET"])
 def findWord():
-    tokenizer = RegexpTokenizer(r'\w+')
     word = request.args.get('word')
     while True:
         try:
@@ -19,7 +19,7 @@ def findWord():
             defined = random_syns.definition()
             for syn in wordnet.synsets(word):
                 for l in syn.lemmas():
-                    if l.name() != word:
+                    if l.name().lower() != word.lower() and len(l.name())>1 and len(l.name())<20:
                         synonyms.append(l.name())
             if len(synonyms) == 0:
                 stop_words = set(stopwords.words("english"))
@@ -30,8 +30,11 @@ def findWord():
             result_list = list(dict.fromkeys(synonyms))
             result = random.choice(result_list)
             final_result = result.replace("_", " ")
-            return(str(final_result))
-        except Exception as e:
-            return(str(e))
+            #return(str(final_result))
+            myhtml = "<head><link rel='stylesheet' type='text/css' title='regular' href='styles1.css' /><link rel='alternate stylesheet' type='text/css' title='sunset' href='sunset.css'/></head><form action='http://localhost:2000/word' method='get' id='form-altered' style='text-align: center;'><input type='text' name='word' id='word' style='width: 20vw; height: 6vh; padding: 3px; font-size: 2em; margin: 2px;' /><input type='submit' id='submit-form' style='cursor: pointer; padding: 5px; font-size: 1.2em;' /><br><br><label style='font-size: 2em; font-family: Georgia, 'Times New Roman', Times, serif;'>"+str(final_result)+"</label></form>"
+            return myhtml
+        except:
+            myhtml = "<head><link rel='stylesheet' type='text/css' title='regular' href='styles1.css' /><link rel='alternate stylesheet' type='text/css' title='sunset' href='sunset.css'/></head><form action='http://localhost:2000/word' method='get' id='form-altered' style='text-align: center;'><input type='text' name='word' id='word' style='width: 20vw; height: 6vh; padding: 3px; font-size: 2em; margin: 2px;' /><input type='submit' id='submit-form' style='cursor: pointer; padding: 5px; font-size: 1.2em;' /><br><br><label style='font-size: 2em; font-family: Georgia, 'Times New Roman', Times, serif;'>" + str("Try again!")+ "</label></form>"
+            return(myhtml)
 
-app.run(host="0.0.0.0", port=80)
+app.run(debug=True, host="0.0.0.0", port=80)
